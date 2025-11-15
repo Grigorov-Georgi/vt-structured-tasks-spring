510aa8151466cec1bb2ed9a36e01bae5133b6d24
Grigorov-Georgi <georgi.grigorov@limechain.tech>
2025-11-15 12:48:59 +0200

    feat: init spring project

---
+++ src/main/java/io/github/grigorov_georgi/vt_structured_tasks_spring/VtStructuredTasksSpringApplication.java
@@ -0,0 +1,13 @@
+package io.github.grigorov_georgi.vt_structured_tasks_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VtStructuredTasksSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VtStructuredTasksSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/grigorov_georgi/vt_structured_tasks_spring/VtStructuredTasksSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.grigorov_georgi.vt_structured_tasks_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VtStructuredTasksSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
